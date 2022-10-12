429e8f139728f950e3f5525275876b01910432a8
Brian Amaro <Brian.Gonzalez@Solera.com>
2022-10-12 11:29:01 -0500

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ContentResult Index()
+
+        {
+            
+            return Content("Hola, aqui verás las peliculas");
+        }
+
+    }
+}
